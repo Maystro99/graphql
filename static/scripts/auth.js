4f4b17f15f2aa6f600b79e1233c16a42033461cd
Maystro <imanwar.alghanem@gamil.com>
2025-12-26 20:10:31 +0300

    Login is Updated

--- static/scripts/auth.js
+++ static/scripts/auth.js
@@ -1,28 +1,37 @@
 const LOGINAPI = "https://learn.reboot01.com/api/auth/signin";
 
-function login() {
-  const login = document.getElementById("login").value;
+const login = async () => {
+  const identifier = document.getElementById("login").value;
   const password = document.getElementById("password").value;
 
-  fetch(LOGINAPI, {
-    method: "POST",
-    headers: {
-      Authorization: "Basic " + btoa(`${login}:${password}`),
-    },
-  })
-    .then((res) => {
-      if (!res.ok) throw new Error("Invalid credentials");
-      return res.json();
-    })
-    .then((token) => {
-      localStorage.setItem("jwt", token);
-    })
-    .catch((err) => {
-      document.getElementById("error").innerText = err.message;
+  try {
+    const res = await fetch(LOGINAPI, {
+      method: "POST",
+      headers: {
+        Authorization: "Basic " + btoa(`${identifier}:${password}`),
+      },
     });
-}
+
+    if (!res.ok) {
+      throw new Error("Invalid credentials");
+    }
+
+    const token = await res.json();
+
+    localStorage.setItem("jwt", token);
+
+    console.log("🎉 Login successful");
+
+    window.location.href = "/templates/profile.html";
+  } catch (err) {
+    const error = document.createElement("p");
+    error.id = "error";
+    error.classList.add("error");
+    error.innerText = err.message;
+  }
+};
 
 // function logout() {
 //   localStorage.removeItem("jwt");
-//   window.location.href = "index.html";
+//   window.location.href = "/index.html";
 // }
